dbb36d302b867711fd929e6350822ba43e5cc050
Celo Composer <support@celo.org>
2026-05-03 18:17:09 +0300

    more updates on fc miniapp

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019dee66-24a8-dc8c-f04f-65fcb288fc67',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
